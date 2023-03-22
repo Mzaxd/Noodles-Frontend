2ad35389f9246a2ff29a23de492d3aa4ee5a4821
Mzaxd <caihaohan0712@foxmail.com>
2023-03-22 20:17:27 +0800

    修改Footer的跳转链接

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -12,7 +12,7 @@
         class="mx-1"
       />
       By <a
-        href="https://pixinvent.com"
+        href="https://github.com/Mzaxd"
         target="_blank"
         rel="noopener noreferrer"
         class="text-primary ms-1"
